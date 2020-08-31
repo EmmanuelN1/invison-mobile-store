0d5fbeb60d226d44e685c400ecc46efc1de0851b
EmmanuelN1 <55398689+EmmanuelN1@users.noreply.github.com>
2020-08-31 02:13:00 +0100

    Updates

---
+++ precache-manifest.37f790abb471dc780d0acdba63672a2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c243b982cbb381061173edbc5f8049ed",
+    "url": "/invison-mobile-store/index.html"
+  },
+  {
+    "revision": "7188427f523a629e6c69",
+    "url": "/invison-mobile-store/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bf76fa76a4abe5f704a4",
+    "url": "/invison-mobile-store/static/css/main.b5d214ce.chunk.css"
+  },
+  {
+    "revision": "7188427f523a629e6c69",
+    "url": "/invison-mobile-store/static/js/2.ff99e34e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/invison-mobile-store/static/js/2.ff99e34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf76fa76a4abe5f704a4",
+    "url": "/invison-mobile-store/static/js/main.cce3f235.chunk.js"
+  },
+  {
+    "revision": "f2a134d0a123b721f2a0",
+    "url": "/invison-mobile-store/static/js/runtime-main.2cd28ddc.js"
+  },
+  {
+    "revision": "300494e9857b8d865ead79a62b09dd88",
+    "url": "/invison-mobile-store/static/media/invision.300494e9.png"
+  }
+]);
